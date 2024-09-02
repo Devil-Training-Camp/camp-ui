12cb440de493f76c9482670a82bfeb74652001be
VincentFong <47265000+newVincentFong@users.noreply.github.com>
2024-09-02 16:49:08 +0800

    feat: add shadow variables

--- components/Theme/base.ts
+++ components/Theme/base.ts
@@ -2,4 +2,8 @@ export const base = {
   'base-border-radius-sm': '4px',
   'base-border-radius-md': '6px',
   'base-border-radius-lg': '8px',
+
+  'base-shadow-sm': '0px 1px 2px 0px rgba(16, 24, 40, 0.06)',
+  'base-shadow-md': '0px 1px 3px 0px rgba(16, 24, 40, 0.1)',
+  'base-shadow-lg': '0px 1px 6px 0px rgba(16, 24, 40, 0.1)',
 }
